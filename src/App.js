aa4b08c85b3fa25e2d17213b44154731d9b8e5e4
Nitish Yadav <nitish.yadav@mosaicwellness.in>
2021-03-20 17:42:29 +0530

    new feature added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          1234
+          I am the new feauture added.
         </p>
         <a
           className="App-link"
